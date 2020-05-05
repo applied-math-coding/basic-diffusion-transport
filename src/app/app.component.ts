293bf589f8995a061f60cbe7424e5479e963776f
Your Name <you@example.com>
2020-05-05 15:13:05 +0300

    init primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { mat } from 'lina';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'basic-diffusion-transport';
+
+  ngOnInit() {
+    const A = mat(2, 2).fill(7);
+    A.print();
+    //TODO  run simulation in webworker and message in reg. timesteps the main to rerender data
+  }
+
+  handleStartSimulation() {
+
+  }
 }
