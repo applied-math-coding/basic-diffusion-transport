e917dd2f1e5e4f70c1bdd8e9a127dc9d33a8036b
Your Name <you@example.com>
2020-05-13 16:14:31 +0300

    op.comp

--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -18,6 +18,7 @@ const c_p = 1.0; //  heat-capacity TODO
 const T_c = 273.15; // K  cold-temp
 const T_h = T_c + 20; // K hot-temp
 const v_max = 2; // m/s^2 max. abs. velocity
+const duration = 60; // s duration
 
 const alpha = k / (rho * c_p);
 const delta_t = Math.min(1 / 4 * delta_x ** 2 / alpha, delta_x / v_max);
@@ -42,15 +43,20 @@ function diffusion_y_op(u: Matrix): Matrix {
   return calc<Matrix>`${u} + ${alpha * delta_t / delta_x ** 2} * (${u.shift_y(-1)} - 2 * ${u} + ${u.shift_y(1)})`;
 }
 
-const u_before = mat(n_grid - 2, n_grid - 2).wrap(1).fill(1);
-const t = diffusion_y_op(diffusion_x_op(u_before));  //TODO try to use the chain from below
-t;
-debugger //TODO  the result is wrong!!! must be 1
-console.log(diffusion_x_op(mat(3, 3).fill(1)).print());
+let u_before = mat(n_grid - 2, n_grid - 2).wrap(1).fill(1);
+for (let t = 0; t <= duration; t = t + delta_t) {
+  u_before = op(diffusion_y_op).comp(diffusion_x_op)(u_before);
+}
 
-function chain<T = any>(f: (v: T) => T): (v: T) => T {
+console.log(u_before.print());
+
+
+//TODO put this into a lib (either PDE lib or functional.lib) what more concepts? memoize, curry, partial, ...
+// chain/pipe (would be reverse of comp)  In(x).appl(f).appl(g) ...
+type Fn<T, R> = (v: T) => R;
+type Op<T, R> = Fn<T, R> & { comp: <S>(g: Fn<S, T>) => Op<S, R> };
+function op<T = any, R = any>(f: Fn<T, R>): Op<T, R> {
   const res = (v: T) => f(v);
-  res['chain'] = (g: (v: T) => T) => chain((v: T) => f(g(v)));
+  res['comp'] = <S>(g: Fn<S, T>) => op((v: S) => f(g(v)));
   return res;
 }
-//TODO  chain(f).chain(g)   this is something what goes into a PDE lib
